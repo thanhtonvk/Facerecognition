976d04b1111215f9031e2590c2f7a74a08a9334e
tondt <tondt@dyno.vn>
2024-06-05 22:05:39 +0700

    add

--- code_ardu/run.ino
+++ code_ardu/run.ino
@@ -1,9 +1,17 @@
+
+
+
 byte ledPin[] = { 2, 3, 4, 5, 6, 7 };
 byte pinCount;
 void setup() {
   Serial.begin(280301);
   Serial.setTimeout(1);
   pinCount = sizeof(ledPin);
+
+  for (int i = 0; i < pinCount; i += 1) {
+    digitalWrite(ledPin[i], HIGH);
+    delay(500);
+  }
   for (int i = 0; i < pinCount; i += 1) {
     digitalWrite(ledPin[i], LOW);
     delay(500);
@@ -14,7 +22,11 @@ void loop() {
 
   if (Serial.available() > 0) {
     x = Serial.readString().toInt();
+
+
     digitalWrite(ledPin[x - 1], HIGH);
+
+
     Serial.println('mo khoa '+ledPin[x - 1]);
     delay(3000);
     // đóng ổ khóa
